b9eed18d44270789813718c5fe482a28b7458c48
guosong <guosong@smarthink.cc>
2023-11-17 11:03:41 +0800

    feat: init project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # aatrox-pub
+
+
+外网路由转发网关
+
+./rcu_task
+
+ps  -aux |grep -v grep|grep 3730976
---
+++ app.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: onefeng
+@time: 2023/11/17 9:59
+"""
+
+# -*- coding: utf-8 -*-
+
+"""
+@author: onefeng
+@time: 2023/11/15 18:24
+"""
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+base_url = 'http://1.15.182.65:20004/'
+
+
+@app.route("/note", methods=["GET"])
+def get_data_by_note():
+    note_id = request.args.get('note_id')
+    url = base_url + '/api/storage/note'
+    data = {
+        'note_id': note_id
+    }
+    r = requests.get(url, params=data)
+    print(r.json())
+    return jsonify(r.json())
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
